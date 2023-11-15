1671950b6bd2c7a4cee088c9a2470c5b528edb30
marrgarett <sb6560259101@lru.ac.th>
2023-11-15 11:48:01 +0700

    15Nov23

--- admin/login.php
+++ admin/login.php
@@ -113,7 +113,7 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form class="user">
+                                    <form class="user" form name="myForm" method="post">
                                         <div class="form-group">
                                             <input type="email" name="username" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
