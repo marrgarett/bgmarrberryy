add73b104d35045217de5ef68184d7e52147d221
marrgarett <sb6560259101@lru.ac.th>
2023-11-27 22:12:42 +0700

    27Nov23 แก้ db ลบ img ออกจาก tbladmin

--- db_bgmarrberryy.sql
+++ db_bgmarrberryy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2023 at 06:20 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Nov 27, 2023 at 04:12 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,16 +36,14 @@ CREATE TABLE `bgmarr_tbl` (
   `bgmarr_price` varchar(255) NOT NULL,
   `bgmarr_img` varchar(255) NOT NULL,
   `bgmarr_status` varchar(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `bgmarr_tbl`
 --
 
 INSERT INTO `bgmarr_tbl` (`bgmarr_id`, `bgmarr_name`, `bgmarr_desc`, `bgmarr_us`, `bgmarr_pw`, `bgmarr_price`, `bgmarr_img`, `bgmarr_status`) VALUES
-(1, 'BGMARR001', 'ของแทร่', 'u53rn4me', 'p@ssw0rd', '10', 'id1.png', 'on'),
-(2, 'BGMARR002', 'yyuuu', 'loke111', '123456', '15', 'pop.pmg', 'off'),
-(61, 'bg93', 'n4', 'ty124', '12345', '12', '', 'on');
+(1, 'BGMARR001', 'ของแทร่ x2', 'u53rn4me', 'p@ssw0rd', '10', 'id1.png', '1');
 
 -- --------------------------------------------------------
 
@@ -63,7 +61,7 @@ CREATE TABLE `history_tbl` (
   `his_end` datetime NOT NULL,
   `his_payment` text NOT NULL,
   `his_status` varchar(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -73,20 +71,19 @@ CREATE TABLE `history_tbl` (
 
 CREATE TABLE `tbladmin` (
   `id` int(11) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `useremail` varchar(255) NOT NULL,
-  `pass_word` varchar(255) NOT NULL,
-  `regdate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `img` varchar(255) NOT NULL
+  `fullname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `useremail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pass_word` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `regdate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `tbladmin`
 --
 
-INSERT INTO `tbladmin` (`id`, `fullname`, `username`, `useremail`, `pass_word`, `regdate`, `img`) VALUES
-(1, 'kritsana tuntipong', 'mgret', 'sb6560259101@lru.ac.th', '71e599d4143772355df19ec76aa14b15', '2023-11-15 03:33:59', 'pic.jpeg');
+INSERT INTO `tbladmin` (`id`, `fullname`, `username`, `useremail`, `pass_word`, `regdate`) VALUES
+(1, 'kritsana tuntipong', 'mgret', 'sb6560259101@lru.ac.th', '71e599d4143772355df19ec76aa14b15', '2023-11-15 03:33:59');
 
 -- --------------------------------------------------------
 
@@ -96,11 +93,11 @@ INSERT INTO `tbladmin` (`id`, `fullname`, `username`, `useremail`, `pass_word`, 
 
 CREATE TABLE `tblclient` (
   `id` int(11) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `useremail` varchar(255) NOT NULL,
-  `pass_word` varchar(255) NOT NULL,
+  `fullname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `useremail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pass_word` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `regdate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `img` varchar(255) NOT NULL
+  `img` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -109,7 +106,8 @@ CREATE TABLE `tblclient` (
 
 INSERT INTO `tblclient` (`id`, `fullname`, `useremail`, `pass_word`, `regdate`, `img`) VALUES
 (2, 'client number0', 'clienttest@gmail.com', 'client', '2023-11-15 05:03:41', 'pic.jpeg'),
-(44, 'qwe', 'qq@gmail.com', '123', '2023-11-25 13:13:35', '');
+(44, 'qwe', 'qq@gmail.com', '123', '2023-11-25 13:13:35', ''),
+(45, 'firstname lastname', 'testregister@gmail.com', 'test', '2023-11-27 14:09:09', '');
 
 --
 -- Indexes for dumped tables
@@ -163,7 +161,7 @@ ALTER TABLE `tbladmin`
 -- AUTO_INCREMENT for table `tblclient`
 --
 ALTER TABLE `tblclient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
