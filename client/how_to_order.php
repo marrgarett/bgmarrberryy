dd6c0e4285298ac898cc64b67dc27e92c8252f79
marrgarett <sb6560259101@lru.ac.th>
2023-12-17 10:43:58 +0700

    17dec23

--- client/how_to_order.php
+++ client/how_to_order.php
@@ -49,39 +49,10 @@ $fullname = $_SESSION['fullname'];
   <!-- ======= Header ======= -->
   <header id="header" class="header d-flex align-items-center fixed-top">
         <div class="container-fluid d-flex align-items-center justify-content-between">
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <?php
-                    if ($fullname == '') {
-                        print '<a href="../admin/login.php">Login</a>';
-                        print '<a>//</a>';
-                        print '<a href="../admin/register.php">Register</a>';
-                    } else {
-                        print '<a href="details_acc.php"><i class="bi bi-person-circle" width="25px" style="margin-right: 5px;"></i>Welcome // ' . $fullname . '</a>'; // แก้ไขข้อมูลส่วนตัว
-                        print '<a href="logout.php" name="logout" value="Logout">logout</a>'; // ล็อคเอาท์ออกจากระบบ
-                    }
-                    ?>
-
-                </ul>
-            </nav>
-            <nav id="navbar" class="navbar">
-                <ul>
-
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="all_id.php">All ID</a></li>
-                    <a href="index.php" class="logo d-flex align-items-center  me-auto me-lg-0">
-                        <img src="img/BG_Logo1.png" alt="">
-                    </a>
-                    <li><a href="how_to_order.php" class="active">How To Order</a></li>
-                    <li><a href="https://www.facebook.com/bgmarrberryy" target="_blank">Contact Us</a></li>
-                </ul>
-            </nav><!-- .navbar -->
-            <div class="header-social-links">
-                <input type="text" class="sidebar-search" placeholder="Search...">
-                <button class="bi bi-search"></button>
-            </div>
-            <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-            <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+        
+          <!-- Start.navbar -->
+          <?php include_once 'sideMenu.php'; ?>
+            <!-- End.navbar -->
 
         </div>
     </header><!-- End Header -->
@@ -104,19 +75,14 @@ $fullname = $_SESSION['fullname'];
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-            <div class="container">
-                <div class="copyright">
-                    &copy; Copyright <strong><span>BGMarrBerryy</span></strong>.
-                </div>
-            </div>
-        </footer><!-- End Footer -->
+    <?php include_once 'footer.php'; ?>
+  <!-- End Footer -->
 
   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <div id="preloader">
+  <!-- <div id="preloader">
     <div class="line"></div>
-  </div>
+  </div> -->
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
