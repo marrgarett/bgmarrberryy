7747ff7f494055d9c0d49852e633e62631a3c22e
utariki <fordno123@gmail.com>
2024-01-19 01:44:42 +0700

    19/1/67 discount

--- client/cartSave.php
+++ client/cartSave.php
@@ -15,10 +15,9 @@ if (isset($_GET["bgmarr_name"])) {
     $bgmarr_id = $row['bgmarr_id'];
     $bgmarr_name = $row['bgmarr_name'];
     $bgmarr_price = $row['bgmarr_price'];
-    $total = $bgmarr_price * 1;
 
-    $sql = "INSERT INTO `id_order` (`id`, `id_order`, `user_id`,  `id_bgmarr_name`, `price`, `quantity_hr`, `total`, `date_order`) 
-    VALUES (NULL, '2312000001', '$user_id', '$bgmarr_name', '$bgmarr_price', '1', '$total', current_timestamp());";
+    $sql = "INSERT INTO `id_order` (`id`, `id_order`, `user_id`,  `id_bgmarr_name`, `price`, `quantity_hr`, `date_order`) 
+    VALUES (NULL, '2312000001', '$user_id', '$bgmarr_name', '$bgmarr_price', '1', current_timestamp());";
 
     if (mysqli_query($conn, $sql)) {
         $sql = "UPDATE `bgmarr_tbl` SET `bgmarr_status` = '$status' WHERE `bgmarr_tbl`.`bgmarr_name` = '$bgmarr_name'";
