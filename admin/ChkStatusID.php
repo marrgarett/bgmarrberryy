06679180fcf5f25bcfa65fa0116427f173c426e3
marrgarett <sb6560259101@lru.ac.th>
2024-01-24 13:57:06 +0700

    24jan24 update status pending > in progress

--- admin/ChkStatusID.php
+++ admin/ChkStatusID.php
@@ -2,48 +2,14 @@
 session_start();
 include_once('db_connect.php');
 
-$fullname = $_POST["fullname"];
-$lastname = $_POST["lastname"];
-$name = $fullname. ' ' . $lastname;
-$useremail = $_POST["useremail"];
-
-
 if (isset($_POST['save'])) {
-    $password1 = $_POST["password1"];
-    $password2 = $_POST["password2"];
-    $user_status = "C";
-    $check_email = mysqli_query($conn, 'SELECT * FROM tblclient WHERE useremail = "' . $_POST['useremail'] . '"');
+    $check_status = mysqli_query($conn, 'SELECT * FROM `history_tbl` WHERE his_status = "Pending"');
     
-    if (trim($_POST['password1']) != trim($_POST['password2'])){
-        echo '<script language="javascript">';
-        echo 'alert("รหัสผ่านไม่ตรงกัน"); location.href="javascript:history.go(-1)"';
-        echo '</script>';
-    
-    }else if (mysqli_num_rows($check_email) >= 1) {
-        echo '<script language="javascript">';
-        echo 'alert("อีเมลซ้ำ"); location.href="javascript:history.go(-1)"';
-        echo '</script>';
-    
-    }else{
-        $password = md5($password1);
-        $sql = "INSERT INTO `tblclient` (`id`, `fullname`, `useremail`, `pass_word`, `regdate` , `user_status`)
-            VALUES (NULL, '$name', '$useremail', '$password', current_timestamp() ,'$user_status');";
-        mysqli_query($conn, $sql);
-        echo '<script language="javascript">';
-        echo 'alert("ลงทะเบียนสำเร็จ"); location.href="login.php"';
-        echo '</script>';
-    }
-} elseif (isset($_POST['update'])){
-    $id = $_GET['id'];
-    $fullname = $_POST["fullname"];
-    $lastname = $_POST["lastname"];
-    $name = $fullname. ' ' . $lastname;
-    $useremail = $_POST["useremail"];
-    $sql = "UPDATE `tblclient` SET `fullname` = '$name', `useremail` = '$useremail'
-    WHERE `tblclient`.`id` = '$id' ";
+} elseif (isset($_GET['his_id'])){
+    $his_id = $_GET['his_id'];
+    $sql = "UPDATE `history_tbl` SET `his_status` = '0' WHERE `history_tbl`.`his_id` = $his_id;";
     $result = mysqli_query($conn, $sql);
         echo '<script language="javascript">';
-        echo 'alert("บันทึกข้อมูลเสร็จสิ้น"); location.href="manage_member.php"';
+        echo 'alert("บันทึกข้อมูลเสร็จสิ้น"); location.href="history_log.php"';
         echo '</script>';
-
 }
